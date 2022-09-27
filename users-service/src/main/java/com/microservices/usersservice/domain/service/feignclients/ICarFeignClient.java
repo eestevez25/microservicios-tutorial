d16a5200e5adedd0021e141f45cfa68698a51101
eestevez <eduardo.estevez@pensemos.com>
2022-09-26 23:31:09 -0500

    Se quita la ruta de feignclient ya que ahora todo se hace por medio del gateway

--- users-service/src/main/java/com/microservices/usersservice/domain/service/feignclients/ICarFeignClient.java
+++ users-service/src/main/java/com/microservices/usersservice/domain/service/feignclients/ICarFeignClient.java
@@ -2,14 +2,12 @@ package com.microservices.usersservice.domain.service.feignclients;
 
 import com.microservices.usersservice.domain.dto.CarDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name="car-service", url="http://localhost:8002", path = "/car")
+//@FeignClient(name="car-service", url="http://localhost:8080", path = "/car")
+@FeignClient(name="car-service", path = "/car")
 public interface ICarFeignClient {
 
     @PostMapping("/save")
--- users-service/src/main/java/com/microservices/usersservice/domain/service/feignclients/IMotoFeignClient.java
+++ users-service/src/main/java/com/microservices/usersservice/domain/service/feignclients/IMotoFeignClient.java
@@ -3,14 +3,12 @@ package com.microservices.usersservice.domain.service.feignclients;
 import com.microservices.usersservice.domain.dto.CarDto;
 import com.microservices.usersservice.domain.dto.MotoDto;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@FeignClient(name="moto-service", url="http://localhost:8003", path="/moto")
+//@FeignClient(name="moto-service", url="http://localhost:8080", path="/moto")
+@FeignClient(name="moto-service", path="/moto")
 public interface IMotoFeignClient {
 
     @PostMapping("/save")
